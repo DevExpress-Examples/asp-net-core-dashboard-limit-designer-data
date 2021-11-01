f6a7a12d6a8c81798cf66b1403b9c65812edf356
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-11-01 13:29:50 +0300

    Initial commit

---
+++ CS/Startup.cs
@@ -0,0 +1,136 @@
+using System.Linq;
+using DevExpress.AspNetCore;
+using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using DevExpress.Data.Filtering;
+using DevExpress.DataAccess.Sql;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using WebDashboardAspNetCore.Models;
+
+namespace WebDashboardAspNetCore {
+    public class Startup {
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment) {
+            Configuration = configuration;
+            FileProvider = hostingEnvironment.ContentRootFileProvider;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IFileProvider FileProvider { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            // Configures services to use the Web Dashboard Control.
+            services
+                .AddHttpContextAccessor()
+                .AddDevExpressControls()
+                .AddControllersWithViews();
+
+            services.AddScoped<DashboardConfigurator>((System.IServiceProvider serviceProvider) => {
+                DashboardConfigurator configurator = new DashboardConfigurator();
+                configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("Data/Dashboards").PhysicalPath));
+                configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(Configuration));
+
+                configurator.AllowExecutingCustomSql = true;
+
+                DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+
+                // Registers an SQL data source.
+                DashboardSqlDataSource sqlDataSource = new DashboardSqlDataSource("Categories", "NWindConnectionString");
+                SelectQuery query = SelectQueryFluentBuilder
+                    .AddTable("Categories")
+                    .SelectAllColumnsFromTable()
+                    .Build("Categories");
+                sqlDataSource.Queries.Add(query);
+                dataSourceStorage.RegisterDataSource("sqlDataSource", sqlDataSource.SaveToXml());
+
+                DashboardSqlDataSource sqlDataSourceCustomSQL = new DashboardSqlDataSource("Products", "NWindConnectionString");
+                CustomSqlQuery queryCustomSQL = new CustomSqlQuery() {
+                    Name = "queryCustomSQL",
+                    Sql = "SELECT ProductID, ProductName FROM Products WHERE ProductID <= @queryParamTopID"
+                };
+
+                queryCustomSQL.Parameters.Add(new QueryParameter("queryParamTopID", typeof(DevExpress.DataAccess.Expression),
+                    new DevExpress.DataAccess.Expression("[Parameters.DashboarParamTopID]")));
+
+                sqlDataSourceCustomSQL.Queries.Add(queryCustomSQL);
+                dataSourceStorage.RegisterDataSource("sqlDataSourceCustom", sqlDataSourceCustomSQL.SaveToXml());
+
+                // Registers an Object data source.
+                DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("Invoices") { DataId = "odsInvoices" } ;
+                dataSourceStorage.RegisterDataSource("objDataSource", objDataSource.SaveToXml());
+
+                configurator.SetDataSourceStorage(dataSourceStorage);
+
+                // Applies dynamic filter in the Designer working mode for the DashboardObjectDataSource.
+                configurator.DataLoading += (s, e) => {
+                    var contextAccessor = serviceProvider.GetService<IHttpContextAccessor>();
+                    var workingMode = contextAccessor.HttpContext.Request.Headers["DashboardWorkingMode"];
+
+                    if (e.DashboardId == "Object" && e.DataId == "odsInvoices") {
+                        var data = Invoices.CreateData();
+
+                        if (workingMode == "Designer")
+                            e.Data = data.Where(item => item.Country == "USA");
+                        else
+                            e.Data = data;
+                    }
+                };
+
+                // Applies dynamic filter in the Designer working mode for the DashboardSqlDataSource.
+                configurator.CustomFilterExpression += (s, e) => {
+                    var contextAccessor = serviceProvider.GetService<IHttpContextAccessor>();
+                    var workingMode = contextAccessor.HttpContext.Request.Headers["DashboardWorkingMode"];
+
+                    if (e.DashboardId == "SQL" && e.QueryName == "Categories") {
+                        if (workingMode == "Designer")
+                            e.FilterExpression = CriteriaOperator.Parse("[Categories.CategoryID] = 1");
+                    }
+                };
+
+                // Applies dynamic filter in the Designer working mode for the DashboardSqlDataSource (Custom SQL Query).
+                configurator.CustomParameters += (s, e) => {
+                    var contextAccessor = serviceProvider.GetService<IHttpContextAccessor>();
+                    var workingMode = contextAccessor.HttpContext.Request.Headers["DashboardWorkingMode"];
+
+                    if (e.DashboardId == "SQL (Custom)") {
+                        var topID = (workingMode == "Designer" ? 3 : 1000);
+                        
+                        e.Parameters.Add(new DevExpress.DataAccess.Parameter("DashboarParamTopID", typeof(int), topID));
+                    }
+                };
+
+                return configurator;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if(env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            // Registers the DevExpress middleware.
+            app.UseDevExpressControls();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => {
+                // Maps the dashboard route.
+                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard", "DefaultDashboard");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,44 @@
+# Dashboard for ASP.NET Core - How to limit data displayed in the Designer working mode
+
+This example illustrates how to filter data in the [Designer working mode](https://docs.devexpress.com/Dashboard/116301/web-dashboard/aspnet-web-forms-dashboard-control/designer-and-viewer-modes) to improve the performance by displaying only several records from the datasource.
+
+The [DashboardControl.onOptionChanged](https://docs.devexpress.com/Dashboard/js-DevExpress.Dashboard.DashboardControlOptions#js_devexpress_dashboard_dashboardcontroloptions_onoptionchanged) event is handled to catch the moment when the working mode is changed. The current mode is passed to the server through the [AjaxRemoteService.headers](https://docs.devexpress.com/Dashboard/js-DevExpress.Dashboard.AjaxRemoteService#js_devexpress_dashboard_ajaxremoteservice_headers) dictionary. After that, a data reloding callback is nitiated through the [DashboardControl.reloadData](https://docs.devexpress.com/Dashboard/js-DevExpress.Dashboard.DashboardControl?p=netframework#js_devexpress_dashboard_dashboardcontrol_reloaddata) method call (see also: [Manage an In-Memory Data Cache](https://docs.devexpress.com/Dashboard/400983/web-dashboard/dashboard-backend/manage-an-in-memory-data-cache)).
+
+On the server side the [IHttpContextAccessor](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-context?view=aspnetcore-3.0) with dependency injection is used to access the passed working mode(`HttpContext.Request.Headers["DashboardWorkingMode"]`) in code.
+
+This example illustrates how to filter data in the following data sources:
+
+* [DashboardObjectDataSource](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DashboardObjectDataSource)
+The [DashboardConfigurator.DataLoading](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWeb.DashboardConfigurator.DataLoading) event is handled for this purpose.
+
+* [DashboardSqlDataSource](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DashboardSqlDataSource) with the regular [SelectQuery](https://docs.devexpress.com/CoreLibraries/DevExpress.DataAccess.Sql.SelectQuery)
+The [DashboardConfigurator.CustomFilterExpression](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWeb.DashboardConfigurator.CustomFilterExpression) event is handed for this purpose.
+
+* [DashboardSqlDataSource](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.DashboardSqlDataSource) with the custom SQL query - [CustomSqlQuery](https://docs.devexpress.com/CoreLibraries/DevExpress.DataAccess.Sql.CustomSqlQuery)
+Custom SQL query contains a parameter (`queryParamTopID`) mapped to the [Dashboard Parameter](https://docs.devexpress.com/Dashboard/117062/web-dashboard/create-dashboards-on-the-web/data-analysis/dashboard-parameters?p=netframework) (see [Pass Parameter Values](https://docs.devexpress.com/Dashboard/117775/web-dashboard/create-dashboards-on-the-web/data-analysis/dashboard-parameters/pass-parameter-values) and [Pass Query Parameters](https://docs.devexpress.com/Dashboard/117192/web-dashboard/create-dashboards-on-the-web/providing-data/working-with-sql-data-sources/pass-query-parameters)). The dashboard parameter and its values are generated dynamically in the [DashboardConfigurator.CustomParameters](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWeb.DashboardConfigurator.CustomParameters) event handler.
+
+
+<!-- default file list -->
+*Files to look at*:
+
+* [Startup.cs](./CS/Startup.cs)
+* [Index.cshtml](./CS/Views/Home/Index.cshtml)
+
+<!-- default file list end -->
+
+## Documentation
+
+- [Designer and Viewer Modes](https://docs.devexpress.com/Dashboard/116301/web-dashboard/aspnet-web-forms-dashboard-control/designer-and-viewer-modes)
+- [Manage an In-Memory Data Cache](https://docs.devexpress.com/Dashboard/400983/web-dashboard/dashboard-backend/manage-an-in-memory-data-cache)
+- [Dashboard Parameters](https://docs.devexpress.com/Dashboard/117062/web-dashboard/create-dashboards-on-the-web/data-analysis/dashboard-parameters?p=netframework)
+- [Pass Parameter Values](https://docs.devexpress.com/Dashboard/117775/web-dashboard/create-dashboards-on-the-web/data-analysis/dashboard-parameters/pass-parameter-values)
+- [Pass Query Parameters](https://docs.devexpress.com/Dashboard/117192/web-dashboard/create-dashboards-on-the-web/providing-data/working-with-sql-data-sources/pass-query-parameters)
+
+## More Examples
+
+- [Dashboard for ASP.NET Core - How to implement authentication](https://github.com/DevExpress-Examples/ASPNET-Core-Dashboard-Authentication)
+- [Dashboard for ASP.NET Core - How to implement multi-tenant Dashboard architecture](https://github.com/DevExpress-Examples/DashboardUserBasedAspNetCore)
+- [Dashboard for ASP.NET Core - How to load different data based on the current user](https://github.com/DevExpress-Examples/DashboardDifferentUserDataAspNetCore)
+
+## See also
+[Web Dashboard - How to research performance issues (a lot of time to display a dashboard or perform a specific action)](https://supportcenter.devexpress.com/ticket/details/t754184)
